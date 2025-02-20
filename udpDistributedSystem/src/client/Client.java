8a610fe94e83f5cde238bd2b28cc3fb5a957c421
Roberto Garcia <159673089+jssroberto@users.noreply.github.com>
2025-02-20 14:15:25 -0700

    Finished Client GUI
    Added validations and separated logic

--- udpDistributedSystem/src/client/Client.java
+++ udpDistributedSystem/src/client/Client.java
@@ -4,7 +4,6 @@
  */
 package client;
 
-import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 
 import javax.swing.*;
@@ -12,38 +11,24 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
-import java.util.Scanner;
 import java.util.logging.Logger;
 
 /**
- *
  * @author carlo
  */
 public class Client {
-    private static Logger logger = Logger.getLogger(Client.class.getName());
+    private static final Logger logger = Logger.getLogger(Client.class.getName());
 
-    public static void main(String[] args) throws SocketException, IOException {
+    public static void main(String[] args) throws IOException {
         setLookAndFeel();
-        JOptionPane.showMessageDialog(null, "Client is running");
+
         DatagramSocket udpSocket = new DatagramSocket();
-
-        // text to send
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Type your name: ");
-        String nombre = sc.nextLine();
-
-        System.out.println("Type your weight (kg): ");
-        Double peso = sc.nextDouble();
-
-        System.out.println("Type your height (m): ");
-        Double altura = sc.nextDouble();
-
         // make object
-        Person p = new Person(nombre, peso, altura);
+        Person person = getUserInput();
 
         // object to csv
-        String message = p.getName() + "," + p.getWeight() + "," + p.getHeight();
+        String message = personToCsv(person);
+
         byte[] buffer = message.getBytes();
 
         // send to middleware
@@ -58,8 +43,8 @@ public class Client {
 
         String serverResponse = new String(responsePacket.getData(), 0, responsePacket.getLength());
 
-        System.out.println("Server Response: " + serverResponse);
-        
+        showServerResponse(serverResponse);
+
         // close socket
         udpSocket.close();
     }
@@ -70,5 +55,53 @@ public class Client {
         } catch (Exception e) {
             logger.severe("Error setting FlatLaf theme");
         }
+    }
+
+    private static Person getUserInput() {
+        String name = getInput("Enter your name");
+        double weight = getValidDoubleInput("Enter your weight (kg)");
+        double height = getValidDoubleInput("Enter your height (m)");
+        return new Person(name, weight, height);
+    }
+
+    private static String personToCsv(Person person) {
+        return person.getName() + "," + person.getWeight() + "," + person.getHeight();
+    }
+
+    private static String getInput(String message) {
+        String input = JOptionPane.showInputDialog(null, message, "IMC Calculator", JOptionPane.QUESTION_MESSAGE);
+        return input != null ? input.trim() : "";
+    }
+
+    private static double getValidDoubleInput(String message) {
+        double value = -1;
+        while (value < 0) {
+            try {
+                String input = getInput(message);
+                value = Double.parseDouble(input);
+                if (value <= 0) {
+                    JOptionPane.showMessageDialog(null, "Please enter a positive number.", "Invalid Input", JOptionPane.ERROR_MESSAGE);
+                    value = -1;
+                }
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Please enter a valid number.", "Invalid Input", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        return value;
+    }
+
+    private static void showServerResponse(String serverResponse) {
+        String[] responseParts = serverResponse.split(",");
+
+        String name = responseParts[0];
+        double weight = Double.parseDouble(responseParts[1]);
+        double height = Double.parseDouble(responseParts[2]);
+        double imc = Double.parseDouble(responseParts[3]);
+        String status = responseParts[4];
+
+        String message = String.format("<html><font size='4'><b>IMC: %.2f</b><br>Name: %s<br>Weight: %.2f kg<br>Height: %.2f m<br>Status: %s</font></html>",
+                imc, name, weight, height, status);
+
+        JOptionPane.showMessageDialog(null, message, "Server Response", JOptionPane.INFORMATION_MESSAGE);
     }
 }
