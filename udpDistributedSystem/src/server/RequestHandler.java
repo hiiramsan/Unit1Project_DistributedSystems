4d11a0e749c03e58a0cf242e45e30f7f49acfe0e
OscarMinjarez <minjarez.dev@gmail.com>
2025-02-20 20:22:36 -0700

    Server working

---
+++ udpDistributedSystem/src/server/RequestHandler.java
@@ -0,0 +1,49 @@
+package server;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class RequestHandler implements Runnable {
+
+    private final DatagramPacket packet;
+
+    public RequestHandler(DatagramPacket packet) {
+        this.packet = packet;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String decodedMessage = new String(this.packet.getData(), 0, this.packet.getLength());
+            String[] data = decodedMessage.split(",");
+            if (data.length != 3) {
+                System.out.println("Invalid format...");
+                return;
+            }
+            String name = data[0];
+            double weight = Double.parseDouble(data[1]);
+            double height = Double.parseDouble(data[2]);
+            double bmi = weight / (height * height);
+            String range = determineBMItring(bmi);
+            String result = name + "," + weight + "," + height + "," + bmi + "," + range;
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+            byte[] response = result.getBytes();
+            DatagramSocket responseSocket = new DatagramSocket();
+            DatagramPacket responsePacket = new DatagramPacket(response, response.length, address, port);
+            responseSocket.send(responsePacket);
+            responseSocket.close();
+            System.out.println("Request: " + name + " with BMI: " + bmi);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String determineBMItring(double bmi) {
+        if (bmi < 18.5) return "thin";
+        if (bmi >= 18.5 && bmi <= 24.9) return "healthy";
+        if (bmi >= 25 && bmi <= 29.9) return "overweight";
+        return "obese";
+    }
+}
--- udpDistributedSystem/src/server/Server.java
+++ udpDistributedSystem/src/server/Server.java
@@ -12,6 +12,8 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  *
@@ -19,62 +21,25 @@ import java.net.SocketException;
  */
 public class Server {
 
+    private static final int PORT = 1001;
+    private static final int BUFFER_SIZE = 2000;
+    private static final int THREAD_POOL_SIZE = 10;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        // UDP Socket on port 1001
-        DatagramSocket udpSocket = new DatagramSocket(1001);
-        while (true) {
-
-            // the buffer or basket for the data
-            byte[] buffer = new byte[2000];
-            // make the packet and receive
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            System.out.println("Waiting to receive...");
-            udpSocket.receive(packet);
-
-            // get message
-            String decodedMessage = new String(packet.getData(), 0, packet.getLength());
-
-            // parse the decodedmessage
-            String[] data = decodedMessage.split(",");
-            if (data.length != 3) {
-                System.out.println("Invalid format");
-                udpSocket.close();
-                return;
+        try (
+            DatagramSocket udpSocket = new DatagramSocket(Server.PORT);
+        ) {
+            ExecutorService threadPool = Executors.newFixedThreadPool(Server.THREAD_POOL_SIZE);
+            System.out.println("UDP server running on port " + Server.PORT);
+            while (true) {
+                byte[] buffer = new byte[BUFFER_SIZE];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                udpSocket.receive(packet);
+                threadPool.execute(new RequestHandler(packet));
             }
-
-            String name = data[0];
-            double weight = Double.parseDouble(data[1]);
-            double height = Double.parseDouble(data[2]);
-
-            // calculate BMI
-            double bmi = weight / (height * height);
-            String range = "";
-
-            if (bmi < 18.5) {
-                range = "thin";
-            } else if (bmi >= 18.5 && bmi <= 24.9) {
-                range = "healthy";
-            } else if (bmi >= 25 && bmi <= 29.9) {
-                range = "overweight";
-            } else {
-                range = "obese";
-            }
-
-            String result = name + "," + weight + "," + height + "," + bmi + "," + range;
-
-            // send back the data
-            InetAddress address = packet.getAddress();
-            int port = packet.getPort();
-
-            byte[] response = result.getBytes();
-
-            DatagramPacket responsePacket = new DatagramPacket(response, response.length, address, port);
-
-            udpSocket.send(responsePacket);
         }
     }
-
 }
