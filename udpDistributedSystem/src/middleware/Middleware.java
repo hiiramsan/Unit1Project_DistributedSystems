3ce7b3933f57adb71745477718f28468dbe9f80a
cristicn2024 <eliana.monge247782@potros.itson.edu.mx>
2025-02-20 17:19:17 -0700

    Middleware finished
    Middleware supports multiple users and effectively communicates between layers.

--- udpDistributedSystem/src/middleware/Middleware.java
+++ udpDistributedSystem/src/middleware/Middleware.java
@@ -8,52 +8,83 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
 
-/**
- *
- * @author carlo
- */
 public class Middleware {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws SocketException, IOException {
+    public static void main(String[] args) {
+        try (DatagramSocket udpSocket = new DatagramSocket(1002)) {
+            while (true) {
+                
+                DatagramPacket packet = receiveClientRequest(udpSocket);
+                new Thread(new RequestHandler(packet, udpSocket)).start();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    
+    private static DatagramPacket receiveClientRequest(DatagramSocket udpSocket) throws IOException {
+        byte[] buffer = new byte[2000];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        System.out.println("Waiting to receive requests...");
+        udpSocket.receive(packet);
+        return packet;
+    }
+
+    
+    static class RequestHandler implements Runnable {
+        private DatagramPacket packet;
+        private DatagramSocket udpSocket;
+
+        public RequestHandler(DatagramPacket packet, DatagramSocket udpSocket) {
+            this.packet = packet;
+            this.udpSocket = udpSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                
+                String clientMessage = processClientRequest();
+                System.out.println("Middleware received: " + clientMessage);
+                
+               
+                String serverMessage = forwardToServer(clientMessage);
+                
+                
+                sendResponseToClient(serverMessage);
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         
-        DatagramSocket udpSocket = new DatagramSocket(1002);
-        while(true) {
-            byte[] buffer = new byte[2000];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            System.out.println("Waiting to receive requests...");
-            udpSocket.receive(packet);
-            
-            // receive clietn reqs
-            String clientMessage = new String(packet.getData(), 0, packet.getLength());
-            System.out.println("Middleware received: " + clientMessage);
-            
-            // send it to server
-            DatagramSocket fwSocket = new DatagramSocket();
-            DatagramPacket serverPacket = new DatagramPacket(clientMessage.getBytes(), clientMessage.length(), InetAddress.getLocalHost(), 1001);
-            fwSocket.send(serverPacket);
-            
-            // receive from server
-             byte[] serverBuffer = new byte[2000];
-            DatagramPacket serverResponse = new DatagramPacket(serverBuffer, serverBuffer.length);
-            fwSocket.receive(serverResponse);
-            fwSocket.close();
-            
-            String serverMessage = new String(serverResponse.getData(), 0, serverResponse.getLength());
-            
-            // send response back to the Client
+        private String processClientRequest() {
+            return new String(packet.getData(), 0, packet.getLength());
+        }
+
+        
+        private String forwardToServer(String message) throws IOException {
+            DatagramPacket serverResponse;
+            try (DatagramSocket fwSocket = new DatagramSocket()) {
+                byte[] messageBytes = message.getBytes();
+                DatagramPacket serverPacket = new DatagramPacket(messageBytes, messageBytes.length, InetAddress.getLocalHost(), 1001);
+                fwSocket.send(serverPacket);
+                byte[] serverBuffer = new byte[2000];
+                serverResponse = new DatagramPacket(serverBuffer, serverBuffer.length);
+                fwSocket.receive(serverResponse);
+            }
+            return new String(serverResponse.getData(), 0, serverResponse.getLength());
+        }
+
+        
+        private void sendResponseToClient(String serverMessage) throws IOException {
             InetAddress clientAddress = packet.getAddress();
             int clientPort = packet.getPort();
             DatagramPacket responsePacket = new DatagramPacket(serverMessage.getBytes(), serverMessage.length(), clientAddress, clientPort);
             udpSocket.send(responsePacket);
-            
         }
-        
-        
     }
-    
 }
